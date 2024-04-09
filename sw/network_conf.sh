f3cbf6ff59fdbb441da316b3325caa0630ef1570
Tal.Halpern <talhlp55@gmail.com>
2024-04-09 08:58:16 +0300

    switch between eth0 and eth1 roles.
    It turns out that eth1 network device refuses to turn into promiscous
    and pass received packets to the bridge.
    This can be debuged inside the eth1 driver.
    Instead a quicker approach of using eth0 instead is chosen.
    eth0 to be an interface in the bridge with tap0.
    eth1 to be used for ssh.

--- sw/network_conf.sh
+++ sw/network_conf.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 ifconfig eth1 192.165.5.11
+ifconfig eth0 0.0.0.0 promisc up
+
 openvpn --mktun --dev tap0
 ifconfig tap0 up
 brctl addbr br0
-brctl addif br0 eth1
+brctl addif br0 eth0
 brctl addif br0 tap0
 ifconfig br0 up
